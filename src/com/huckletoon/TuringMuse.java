6fbb80889ec15404d685e0a690e7d114a5a5036e
Gary <huckletoon@gmail.com>
2019-05-03 13:49:55 -0500

    Tested conversions and output

--- src/com/huckletoon/TuringMuse.java
+++ src/com/huckletoon/TuringMuse.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.huckletoon.grammar.GrammarMachine;
@@ -13,9 +15,10 @@ public class TuringMuse {
 	
 	static String file = "temp.mid";
 	
-	public static byte[] HEADER = {0x4d, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06, 0x00, 0x01, 0x00, 0x02, 0x01, (byte) 0xe0};
+	public static Byte[] HEADER = {0x4d, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06, 0x00, 0x01, 0x00, 0x02, 0x01, (byte) 0xe0};
 	
-	static byte[] data = new byte[] {
+	// Test data used for analysis
+	static Byte[] data = new Byte[] {
 			0x4d, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06, 0x00, 0x01, 0x00, 0x02, 0x01, (byte) 0xe0, 
 			0x4d, 0x54, 0x72, 0x6b, 0x00, 0x00, 0x00, 0x27, 
 				0x00, (byte) 0xff, 0x03, 0x10, 0x50, 0x49, 0x41, 0x4e, 0x4f, 0x20, 0x54, 0x48, 0x45, 0x4d, 0x45, 0x20, 0x38, 0x39, 0x5f, 0x41,
@@ -38,12 +41,9 @@ public class TuringMuse {
 			0x60, 0x00, 0x4c, 0x60, 0x00, 0x51, 0x61, 0x54, 0x45, 0x00, 0x00, 0x49, 0x00, 0x00, 0x4c, 0x00,
 			0x00, 0x51, 0x00, 0x00, (byte) 0xff, 0x2f, 0x00
 	};
-
 	
 	public static void main(String[] args) throws IOException {
-//		FileOutputStream outStr = new FileOutputStream(file);
-//		outStr.write(data);
-//		outStr.close();
+		
 		
 		GrammarMachine gm = new GrammarMachine();
 		List<GrammarProduction> prods = buildDorianD();
@@ -53,8 +53,24 @@ public class TuringMuse {
 		
 		gm.loadSeed("A");
 		gm.runGrammar();
-		System.out.println(gm.getString());
+		String chordProgression = gm.getString();
+		System.out.println(chordProgression);
 		
+		/*
+		 * TODO
+		 * Below is pattern to write to midi file
+		List<Byte> musicData = new ArrayList<>();
+		Collections.addAll(musicData, data);
+		
+		byte[] testData = new byte[musicData.size()];
+		for (int i = 0; i < musicData.size(); i++) {
+			testData[i] = musicData.get(i);
+		}
+
+		FileOutputStream outStr = new FileOutputStream(file);
+		outStr.write(testData);
+		outStr.close();
+		*/
 	}
 	
 	private static List<GrammarProduction> buildDorianD() {
@@ -89,17 +105,17 @@ public class TuringMuse {
 		dProd.addProduction("fMF", 0.05);
 		dProd.addProduction("f+F", 0.05);
 
-		eProd.addProduction("amA", 0.2);
-		eProd.addProduction("a-A", 0.2);
 		eProd.addProduction("cMC", 0.3);
 		eProd.addProduction("c+C", 0.3);
+		eProd.addProduction("amA", 0.2);
+		eProd.addProduction("a-A", 0.2);
 
-		fProd.addProduction("amA", 0.2);
-		fProd.addProduction("amA", 0.2);
 		fProd.addProduction("emE", 0.1);
 		fProd.addProduction("e-E", 0.1);
 		fProd.addProduction("cMC", 0.2);
 		fProd.addProduction("c+C", 0.2);
+		fProd.addProduction("amA", 0.2);
+		fProd.addProduction("amA", 0.2);
 
 		gProd.addProduction("gMG", 0.1);
 		gProd.addProduction("g7G", 0.2);
